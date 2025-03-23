bab35944fc50f9e8a0d5d8699e170557d0be5346
Sam Adams <samuel.grant.adams@gmail.com>
2025-03-23 16:06:47 -0500

    defined headers

---
+++ src/data.cc
@@ -0,0 +1,20 @@
+#ifndef __DATA_H
+#define __DATA_H
+
+#include <fstream>
+#include "stdint.h"
+#include "data.hpp"
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_set>
+
+class data_handler
+{
+    std::vector<data *> *data_array;
+    std::vector<data *> *training_data;
+    std::vector<data *> *training_data;
+    std::vector<data *> *training_data;
+};
+
+#endif
